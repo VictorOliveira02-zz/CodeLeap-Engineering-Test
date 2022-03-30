22c8c0af3e0cfac06fa37a173df8879d8c299d3e
VictorOliveira02 <victoralvees17@gmail.com>
2022-03-30 20:55:00 -0300

    fix css btn signup

--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,19 +1,21 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    
+
     background: #DDDDDD;
+    width: 100%;
+    height: 100%;
+
+    position: fixed;
 
     display: flex;
     justify-content: center;
+    align-items: center;
 `;
 
 export const Div = styled.div`
     width: 500px;
     height: 220px;
-    margin-top: 200px;
     padding: 20px;
 
     background: #FFFFFF;
@@ -38,6 +40,7 @@ export const Div = styled.div`
         border: 1px solid #777777;
         border-radius: 4px;
         margin-bottom: 18px;
+        padding-left: 5px;
         
         ::placeholder {
             padding-left: 5px;
@@ -50,16 +53,16 @@ export const Div = styled.div`
         height: 33px;
         margin-top: 20px;
         margin-left: 76%;
-        cursor: pointer;
-        background: #000000;
+        background-color: black;
+        color: white;
 
-        font-size: 1em;
-        line-height: 1.188em;
-        color: #FFFFFF;
-
+        font-weight: bold;
+        font-size: 16px;
+        
         @media (max-width: 785px) {
             width: 98%;
-            margin-top: 10px ;
+            margin: 0 auto;
+            
         }
     }
 
@@ -75,7 +78,7 @@ export const Div = styled.div`
         color: #FFFFFF;
         cursor:url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>❌</text></svg>") 16 0,auto; /*!emojicursor.app*/
         :hover {
-                color: #AEAFB2;
+            color: #AEAFB2;
         }
 
 
