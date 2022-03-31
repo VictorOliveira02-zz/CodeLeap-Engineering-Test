b69a1cf1494a353b3f8ecb50e278a27c596a0f48
VictorOliveira02 <victoralvees17@gmail.com>
2022-03-30 21:05:36 -0300

    fix widt signup

--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
     background: #DDDDDD;
     width: 100%;
     height: 100%;
@@ -15,8 +14,9 @@ export const Container = styled.div`
 
 export const Div = styled.div`
     width: 500px;
-    height: 220px;
+    height: 30%;
     padding: 20px;
+    min-height: 250px;
 
     background: #FFFFFF;
     border: 2px solid #CCCCCC;
